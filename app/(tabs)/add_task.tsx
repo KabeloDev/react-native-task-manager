46f7390d253ac796b6a46c91e1ad71ec53c50d31
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-07-28 15:53:33 +0200

    Add-Task > Adding task to database

--- app/(tabs)/add_task.tsx
+++ app/(tabs)/add_task.tsx
@@ -1,8 +1,45 @@
+import { database, DATABASE_ID, TASKS_COLLECTION_ID } from '@/app_context/appwrite';
+import { useAuth } from '@/app_context/auth_context';
 import { Image } from 'expo-image';
+import { router } from 'expo-router';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ID } from 'react-native-appwrite';
+import { Button, SegmentedButtons, TextInput, useTheme } from 'react-native-paper';
 
+const FREQUENCY_OPTIONS = ["Daily", "Weekly", "Monthly"];
+type Frequency = (typeof FREQUENCY_OPTIONS)[number];
 
 export default function AddTaskScreen() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [frequency, setFrequency] = useState<Frequency>("Daily");
+    const [error, setError] = useState<string | null>(null);
+    const theme = useTheme();
+    const { user } = useAuth();
+
+    const handleSubmit = async () => {
+        if (!user) return;
+
+        try {
+            await database.createDocument(
+                DATABASE_ID,
+                TASKS_COLLECTION_ID,
+                ID.unique(),
+                {
+                    user_id: user.$id,
+                    title: title,
+                    description: description,
+                    frequency: frequency,
+                    isComplete: false,
+                }
+            );
+            router.replace('/');
+        } catch (error) {
+            setError(error instanceof Error ? error.message : "Failed to add task");
+        }
+
+    }
 
     return (
         <View style={styles.body}>
@@ -10,7 +47,23 @@ export default function AddTaskScreen() {
                 source={require('@/assets/images/partial-react-logo.png')}
                 style={styles.reactLogo}
             />
-            <Text style={styles.titleContainer}>Add Task screen</Text>
+            <View style={styles.taskForm}>
+                <TextInput label="Title" mode="outlined" onChangeText={setTitle} style={ styles.formMargin } />
+                <TextInput label="Description" mode="outlined" multiline numberOfLines={4} style={ styles.formMargin } onChangeText={setDescription} />
+                <SegmentedButtons style={ styles.formMargin }
+                    value={frequency} onValueChange={(value) => setFrequency(value as Frequency)}
+                    buttons={FREQUENCY_OPTIONS.map((item) => ({
+                        value: item,
+                        label: item.charAt(0).toUpperCase() + item.slice(1),
+                        style: { backgroundColor: frequency === item ? '#74b1c2ff' : 'transparent' }
+                    }))} />
+
+                <Button mode="contained" disabled={!title || !description} onPress={handleSubmit} style={styles.addButton}>
+                    Add Habit
+                </Button>
+
+                {error && <Text style={{ color: theme.colors.error, marginTop: 10 }}>{error}</Text>}
+            </View>
         </View>
     );
 }
@@ -18,8 +71,19 @@ export default function AddTaskScreen() {
 const styles = StyleSheet.create({
     body: {
         flex: 1,
+    },
+    taskForm: {
+        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignContent: 'center',
+        padding: 10
+    },
+    formMargin : {
+        marginBottom: 20
+    },
+    addButton : {
+        marginTop: 20,
+        backgroundColor: '#74b1c2ff'
     },
     titleContainer: {
         flexDirection: 'row',
--- type/databases.type.ts
+++ type/databases.type.ts
@@ -4,5 +4,6 @@ export interface Task extends Models.Document {
     user_id: string;
     title: string;
     description: string;
+    frequency: string; 
     isComplete: boolean;
 }
