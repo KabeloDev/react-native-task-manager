462a3ed1ce015ba1c7f53a4f34e306eed35f5059
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-07-28 15:01:45 +0200

    Add-Task > Setting up database

---
+++ type/databases.type.ts
@@ -0,0 +1,8 @@
+import { Models } from 'react-native-appwrite';
+
+export interface Task extends Models.Document {
+    user_id: string;
+    title: string;
+    description: string;
+    isComplete: boolean;
+}
